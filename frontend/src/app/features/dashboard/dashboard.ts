b8c79d9d4b80ba3a1140636e6251324f1623d21f
Ilhomjon0207 <ilhomjonqodirov0207@gmail.com>
2026-07-30 15:55:02 +0500

    add brand color

---
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Button} from "primeng/button";
+
+@Component({
+  selector: 'app-dashboard',
+    imports: [
+        Button
+    ],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss',
+})
+export class Dashboard {
+
+}
