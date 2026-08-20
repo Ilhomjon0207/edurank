5f6018e7ff413c8a917e16c5b6e6afa4748f8856
Ilhomjon Qodorov <111631902+ilhomjon-dev@users.noreply.github.com>
2026-08-20 16:40:32 +0500

    add mobile version to dashboard

--- frontend/src/app/shared/components/recent-applications/recent-applications.ts
+++ frontend/src/app/shared/components/recent-applications/recent-applications.ts
@@ -1,15 +1,31 @@
 import { Component, input } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { IRecentApplicationResponse } from '@/app/core/interfaces';
+import { Tag } from 'primeng/tag';
 
 @Component({
     selector: 'app-recent-applications',
-    imports: [TableModule],
+    imports: [TableModule, Tag],
     templateUrl: './recent-applications.html',
     standalone: true,
     styleUrl: './recent-applications.scss'
 })
 export class RecentApplications {
     applications = input<IRecentApplicationResponse[]>([]);
+    getStatusSeverity(status: string) {
+        switch (status) {
+            case 'ACCEPTED':
+                return 'success';
+
+            case 'REJECTED':
+                return 'danger';
+
+            case 'PENDING':
+                return 'warn';
+
+            default:
+                return 'secondary';
+        }
+    }
     ngOnInit() {}
 }
