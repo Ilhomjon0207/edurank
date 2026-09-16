c0acb09b7e4598062c862766b71b0f1963e71bcc
Ilhomjon0207 <ilhomjonqodirov0207@gmail.com>
2026-09-16 17:13:17 +0500

    add environment.prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backend-production-beb2.up.railway.app/api'
+    apiUrl: 'https://backend-production-beb2.up.railway.app'
 };
